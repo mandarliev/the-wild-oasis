702cdc1491ef3a5d1a57eab5920f6a4588bb54c1
Dimitar <dimitar.mandarliev@gmail.com>
2026-01-09 14:26:35 +0200

    Mutations: Deleting a cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,3 +10,12 @@ export async function getCabins() {
 
   return data;
 }
+
+export async function deleteCabin(id) {
+  const { error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.log("Cabins could not be deleted");
+    throw new Error("Cabins could not be deleted");
+  }
+}
