8ae845f24051d9ef0bb46f066d39bf5437de0392
Dimitar <dimitar.mandarliev@gmail.com>
2026-01-13 20:32:42 +0200

    Uploading images to Supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://jzcmdlftdqiszjhqjhkp.supabase.co";
+export const supabaseUrl = "https://jzcmdlftdqiszjhqjhkp.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6Y21kbGZ0ZHFpc3pqaHFqaGtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4NTE1NzEsImV4cCI6MjA4MzQyNzU3MX0.KPWH4fGezNi-JmUdq0TFixvVkh0cJtzQuDMzuZ_LuTQ";
 const supabase = createClient(supabaseUrl, supabaseKey);
