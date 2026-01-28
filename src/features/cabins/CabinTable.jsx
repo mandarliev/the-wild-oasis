d0fc9dfe9ddddd134d66da86ff1f4ae20ad1c4d1
Dimitar <dimitar.mandarliev@gmail.com>
2026-01-28 10:10:47 +0200

    Building the Bookings Table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
@@ -11,6 +12,9 @@ function CabinTable() {
 
   if (isLoading) {
     return <Spinner />;
+  }
+  if (!cabins.length) {
+    return <Empty resourceName="cabins" />;
   }
 
   // 1) Filter
