ee6171244cbb2a4c9fdadaa988d583a18cc57485
Dimitar <dimitar.mandarliev@gmail.com>
2026-02-06 15:13:20 +0200

    User Sign up

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,24 @@
 import supabase from "./supabase";
 
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
+
 export async function login({ email, password }) {
   let { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -27,7 +46,7 @@ export async function getCurrentUser() {
 }
 
 export async function logout() {
-  const {error} = await supabase.auth.signOut();
+  const { error } = await supabase.auth.signOut();
   if (error) {
     throw new Error(error.message);
   }
