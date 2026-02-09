5a3d985333185c6906930cfaaa5122f5aedd81b1
Dimitar <dimitar.mandarliev@gmail.com>
2026-02-09 14:26:56 +0200

    Implementing Dark Mode with CSS variables

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -1,5 +1,4 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createEditCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
 import { updateCurrentUser } from "../../services/apiAuth";
 
